88ebde37631c33805419928e1ddacdb50b8fef6e
Richard Guerre <guerre.richard@me.com>
2020-01-11 16:40:06 +0100

    Initial commit Setup for making components

---
+++ src/Component/Component.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "./component.css";
+
+function Component() {
+  return (
+    <div className="Component">
+      <div className="title">Title</div>
+      <div className="description">Description</div>
+      <div className="learnMore">Learn more</div>
+    </div>
+  )
+}
+
+export default Component
